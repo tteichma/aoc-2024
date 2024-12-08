a7524eed098ad92541f8ee8119002dbfac974941
Tim Teichmann <TimTeichmann@posteo.net>
2024-12-08 17:08:36 +0100

    Added Day07 part 2.

--- src/Day07.kt
+++ src/Day07.kt
@@ -8,28 +8,27 @@ private class Day07(val equations: List<Pair<Long, List<Long>>>) {
             return Day07(equations)
         }
 
-        fun yieldEquationResults(rhs: List<Long>): Sequence<Long> = sequence {
+        fun yieldEquationResults(rhs: List<Long>, includeConcat: Boolean): Sequence<Long> = sequence {
             if (rhs.size == 1) {
                 yield(rhs.first())
                 return@sequence
             }
 
-            for (result in yieldEquationResults(rhs.dropLast(1))) {
+            for (result in yieldEquationResults(rhs.dropLast(1), includeConcat)) {
                 yield(result + rhs.last())
                 yield(result * rhs.last())
+                if (includeConcat) {
+                    yield("${result}${rhs.last()}".toLong())
+                }
             }
         }
     }
 
-    fun solvePart1(): Long {
+    fun solve(includeConcat: Boolean): Long {
         val solvableEquations = equations.filter { (lhs, rhs) ->
-            yieldEquationResults(rhs).any { it == lhs }
+            yieldEquationResults(rhs, includeConcat).any { it == lhs }
         }
         return solvableEquations.sumOf { it.first }
-    }
-
-    fun solvePart2(): Long {
-        return 0L
     }
 }
 
@@ -38,21 +37,21 @@ fun main() {
     val testInput = readInput("Day07_test")
     profiledCheck(3749L, "Part 1 test") {
         val day = Day07.fromInput(testInput)
-        day.solvePart1()
+        day.solve(false)
     }
-    profiledCheck(0L, "Part 2 test") {
+    profiledCheck(11387L, "Part 2 test") {
         val day = Day07.fromInput(testInput)
-        day.solvePart2()
+        day.solve(true)
     }
 
     // Read the input from the `src/Day07.txt` file.
     val input = readInput("Day07")
     profiledExecute("Part 1") {
         val day = Day07.fromInput(input)
-        day.solvePart1()
+        day.solve(false)
     }.println()
     profiledExecute("Part 2") {
         val day = Day07.fromInput(input)
-        day.solvePart2()
+        day.solve(true)
     }.println()
 }
